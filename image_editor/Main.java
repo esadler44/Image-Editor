06bdb3e91b38d5fd27820dae99cdf6088735b5f5
esadler44 <esadler44@gmail.com>
2013-02-18 13:09:01 -0500

    This is the main program to run.

---
+++ image_editor/Main.java
@@ -0,0 +1,13 @@
+package image_editor;
+
+public class Main extends Object
+{
+   public static void main(String args[])
+   {  
+	  System.out.println("Welcome to my awesome photoshop program!");
+	  System.out.println("Please look at the console for further instructions on what to do.");
+      ITransformations trans = new Transformer();
+      @SuppressWarnings("unused")
+	  ImageTransformerGUI theGUI = new ImageTransformerGUI(trans);
+   }
+}
